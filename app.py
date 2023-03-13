552b382be22bd889e3411f5a6f527280247cce71
joshua <wanhoyinjoshua@yahoo.comgit config --global user.name wanhoyinjoshuagit config --global user.name wanhoyinjoshua>
2023-03-14 01:45:33 +1100

    hi

--- app.py
+++ app.py
@@ -7,12 +7,12 @@
 import spacy
 
 from spacy import displacy
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    textstring="I happy mother boyfriend not happy"
+    textstring=request.args['sentence']
     nlp = spacy.load("en_core_web_sm")
     doc = nlp(textstring)
     html = displacy.render(doc, style='dep', page=True)

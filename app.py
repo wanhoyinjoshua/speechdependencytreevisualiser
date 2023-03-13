bd446bed8ad0d732d4b41509010603dc3034b780
joshua <wanhoyinjoshua@yahoo.comgit config --global user.name wanhoyinjoshuagit config --global user.name wanhoyinjoshua>
2023-03-14 09:21:33 +1100

    hi

--- app.py
+++ app.py
@@ -7,12 +7,20 @@
 import spacy
 
 from spacy import displacy
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    textstring=request.args['sentence']
+
+    html = ""
+
+
+    return render_template('index.html')
+
+@app.route("/anlyse", methods=['GET'])
+def hello():
+    textstring = request.args['projectFilepath']
     nlp = spacy.load("en_core_web_sm")
     doc = nlp(textstring)
     html = displacy.render(doc, style='dep', page=True)
